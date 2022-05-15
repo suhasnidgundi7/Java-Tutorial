551302f2eb7bdd2b35dc0729ee9c8642502e22c3
Suhas Nidgundi <suhasnidgundi@gmail.com>
2022-05-15 11:40:57 +0530

    Write a program to convert Integer object value into primitive datatype byte, short and double value.

---
+++ Project 19/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String a[]){
+        Integer itr = new Integer(10);
+        System.out.println("byte value: "+itr.byteValue());
+        System.out.println("double value: "+itr.doubleValue());
+        System.out.println("float value: "+itr.floatValue());
+        System.out.println("int value: "+itr.intValue());
+        System.out.println("long value: "+itr.longValue());
+        System.out.println("short value: "+itr.shortValue());
+    }
+}
